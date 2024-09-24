e6eddd865cf0c7b502c89c5513266b2298c61b97
sim-mag <79795891+sim-mag@users.noreply.github.com>
2024-09-24 20:44:45 +0200

    updated task 1

--- js/task1.js
+++ js/task1.js
@@ -9,6 +9,8 @@ function isEnoughCapacity(products, containerSize) {
   return totalProducts <= containerSize;
 }
 
+console.log("These are the results of Task 1");
+
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
 console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)); // true
